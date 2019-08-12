903ce04361987a6643d479527a449da9cd77adb1
Sruti Bhattacharjee <SrutiBh@users.noreply.github.com>
2019-08-13 03:31:54 +0530

    Create  results.py

---
+++ samples/results.py
@@ -0,0 +1,6 @@
+# Run detection
+results = model.detect([image], verbose=1)
+
+# Visualize results
+r = results[0]
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
